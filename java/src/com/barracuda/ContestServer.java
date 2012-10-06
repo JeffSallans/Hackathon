fc91a7198204eef9af0edb5e6b42385319f81e08
Benjamin Magee <benrm@umich.edu>
2012-10-06 08:00:13 -0400

    make choice continued

--- java/src/com/barracuda/ContestServer.java
+++ java/src/com/barracuda/ContestServer.java
@@ -70,22 +70,21 @@ public class ContestServer {
         
         System.out.print(temp_elt.ghost_shortest_path(14, place_board, ref_board));
         
-        int index = 0;
+        int ret = 0;
         ArrayList<Target> targets = null;
         {
         	int min = 50;
 	        for(int i = 0; i < targets.size(); i++) {
 	            Target temp = targets.get(i);
-	            if(temp.s_path_to_goal1 + temp.s_path_to_root <= min)
-	            	min = temp.s_path_to_goal1 + temp.s_path_to_root;
-	            if(temp.s_path_to_goal2 + temp.s_path_to_root <= min)
-	            	min = temp.s_path_to_goal2 + temp.s_path_to_root;
-	            index = i;
+	            if(temp.min() < min)
+	            {
+	            	ret = temp.value;
+	            	min = temp.min();
+	            }
 	        }
         }
-        //ArrayList< Target > targets = 
         
-        return offer.get(index);
+        return ret;
     }
 
     public int move_result(Map result) {
--- java/src/com/barracuda/Target.java
+++ java/src/com/barracuda/Target.java
@@ -2,12 +2,19 @@ package com.barracuda;
 
 public class Target extends Element {
 
-    public int offset;
+    public int offset = 0;
 
-    public int s_path_to_goal1;
-    public int s_path_to_goal2;
+    public int s_path_to_goal1 = 0;
+    public int s_path_to_goal2 = 0;
     
-    public int s_path_to_root;
+    public int s_path_to_root = 0;
+    
+    public int min()
+    {
+        if(s_path_to_goal1 < s_path_to_goal2)
+        	return s_path_to_goal1 + s_path_to_root;
+        return s_path_to_goal2 + s_path_to_root;
+    }
     
     public Target(int in_owner, int in_value, int[][] board) {
         super(in_owner, in_value, board);
