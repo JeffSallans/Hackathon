a6d10f22a3dd8b130ce10c949472ba851b73ddb2
Matthew Ripari <mlripper33@gmail.com>
2012-10-06 00:49:27 -0400

    Bidding class start

---
+++ java/src/com/barracuda/Bidding.java
@@ -0,0 +1,16 @@
+package com.barracuda;
+
+public class Bidding 
+{
+    int round_counter;
+    int own_credits;
+    int opp_credits;
+    int own_desire;
+    int opp_desire;
+    
+    Bidding(int rounds)
+    {
+        this.round_counter = rounds;
+    }
+
+}
