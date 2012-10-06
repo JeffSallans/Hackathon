6e14e295e2371a939916b99b98245218dad0578a
Jeff Sallans <jefsallans@yahoo.com>
2012-10-06 03:43:29 -0400

    ghost shortest path complete

--- java/src/com/barracuda/Element.java
+++ java/src/com/barracuda/Element.java
@@ -130,6 +130,171 @@ public class Element {
         }
     }
     
+    //Using a breath-first search to find the shortest path
+    //EFFECT: returns -1 if invalid path will move through owned blocks and not count it as a length
+    //REQUIRE: placement_board empty spots are -1
+    //          placement_board self owned spots are 0
+    public int ghost_shortest_path(int dest, int[][] placement_board, int[][] ref_board) {
+        
+        //Check if dest is value
+        if (dest == value) {
+            return 0;
+        }
+        
+        //Re-assign values in placement_board
+        for(int i = 0; i < 7; i++) {
+            for(int j = 0; j < 7; j++) {
+                
+                //Check if self owned re assigned to -2
+                if (placement_board[i][j] == 0) {
+                    placement_board[i][j] = -2;
+                }
+            }
+        }
+        
+        Element temp_val;
+        Vector< Element > queue = new Vector< Element >();
+        
+        //add value to the first position in the queue
+        queue.add(new Element(-1, value, ref_board));
+        placement_board[x][y] = 1;
+        
+        do {
+            //pop queue element
+            temp_val = queue.remove(0);
+            
+            //Check if dest
+            if (temp_val.value == dest) {
+                break;
+            }
+            
+            //Check and enqueue valid adjacent
+            
+            //north
+            if (temp_val.x > 0 && placement_board[temp_val.x - 1][temp_val.y] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x - 1][temp_val.y] == -1) {
+                    placement_board[temp_val.x - 1][temp_val.y] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x - 1][temp_val.y], ref_board));
+                }
+                //placement_board is -2
+                else {
+                    //self owned spots are not counted with location
+                    placement_board[temp_val.x - 1][temp_val.y] = placement_board[temp_val.x][temp_val.y];
+                    //add to front of queue
+                    queue.add(0, new Element(-1, ref_board[temp_val.x - 1][temp_val.y], ref_board));
+                }
+            }
+            //south
+            else if (temp_val.x < 6 && placement_board[temp_val.x + 1][temp_val.y] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x + 1][temp_val.y] == -1) {
+                    placement_board[temp_val.x + 1][temp_val.y] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x + 1][temp_val.y], ref_board));
+                }
+                //placement_board is -2
+                else {
+                    placement_board[temp_val.x + 1][temp_val.y] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x + 1][temp_val.y], ref_board));
+                }
+            }
+            //east
+            else if (temp_val.y < 6 && placement_board[temp_val.x][temp_val.y + 1] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x][temp_val.y + 1] == -1) {
+                    placement_board[temp_val.x][temp_val.y + 1] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x][temp_val.y + 1], ref_board));
+                }
+                //placement_board is -2
+                else {
+                    placement_board[temp_val.x][temp_val.y + 1] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x][temp_val.y + 1], ref_board));
+                }
+            }
+            //west
+            else if (temp_val.y > 0 && placement_board[temp_val.x][temp_val.y - 1] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x][temp_val.y - 1] == -1) {
+                    placement_board[temp_val.x][temp_val.y - 1] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x][temp_val.y - 1], ref_board));
+                }
+                else {
+                    placement_board[temp_val.x][temp_val.y - 1] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x][temp_val.y - 1], ref_board));
+                }
+            }
+            //ne
+            else if (temp_val.x > 0 && temp_val.y < 6 && placement_board[temp_val.x - 1][temp_val.y + 1] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x - 1][temp_val.y + 1] == -1) {
+                    placement_board[temp_val.x - 1][temp_val.y + 1] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x - 1][temp_val.y + 1], ref_board));
+                }
+                else {
+                    placement_board[temp_val.x - 1][temp_val.y + 1] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x - 1][temp_val.y + 1], ref_board));
+                }
+            }
+            //nw
+            else if (temp_val.x > 0 && temp_val.y > 0 && placement_board[temp_val.x - 1][temp_val.y - 1] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x - 1][temp_val.y - 1] == -1) {
+                    placement_board[temp_val.x - 1][temp_val.y - 1] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x - 1][temp_val.y - 1], ref_board));
+                }
+                else {
+                    placement_board[temp_val.x - 1][temp_val.y - 1] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x - 1][temp_val.y - 1], ref_board));
+                }
+            }
+            //se
+            else if (temp_val.x < 6 && temp_val.y < 6 && placement_board[temp_val.x + 1][temp_val.y + 1] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x + 1][temp_val.y + 1] == -1) {
+                    placement_board[temp_val.x + 1][temp_val.y + 1] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x + 1][temp_val.y + 1], ref_board));
+                }
+                else {
+                    placement_board[temp_val.x + 1][temp_val.y + 1] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x + 1][temp_val.y + 1], ref_board));
+                }
+            }
+            //sw
+            else if (temp_val.x < 6 && temp_val.y > 0 && placement_board[temp_val.x + 1][temp_val.y - 1] <= -1) {
+                
+                //Mark as visited with current distance
+                if (placement_board[temp_val.x + 1][temp_val.y - 1] == -1) {
+                    placement_board[temp_val.x + 1][temp_val.y - 1] = placement_board[temp_val.x][temp_val.y] + 1;
+                    queue.add(new Element(-1, ref_board[temp_val.x + 1][temp_val.y - 1], ref_board));
+                }
+                else {
+                    placement_board[temp_val.x + 1][temp_val.y - 1] = placement_board[temp_val.x][temp_val.y];
+                    queue.add(0, new Element(-1, ref_board[temp_val.x + 1][temp_val.y - 1], ref_board));
+                }
+            }
+        }
+        while(!queue.isEmpty());
+        //While the queue is not empty or found the dest
+        
+        
+        //Check if found
+        if (temp_val.value == dest) {
+            return placement_board[temp_val.x][temp_val.y];
+        }
+        else {
+            return -1;
+        }
+    }
+    
+    //This gets the total number of elements linked
+    //NOT the stretch of a link
     public int longest_link(int[][] ref_board, int[][] placement_board){
         
         //one for the element calling this function
